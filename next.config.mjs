c9bb81abab237c55c44125ab1fbf022e3cdca167
Przemysław Skaliński <przemyslawskalinski@laptop-Przemysaw.local>
2026-02-11 04:13:01 +0100

    chore(config): remove unsupported reactCompiler option

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactCompiler: true,
-
   experimental: {
     serverActions: {
-      // zwiększamy limit do 10 MB
       bodySizeLimit: "10mb",
     },
   },
